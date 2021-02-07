558944be1683709768f93772d6a28910e3b244e9
pikachu5551 <via722jam.emam25@gmail.com>
2021-02-08 04:04:07 +0900

    コメントサーバーのwebsocketURLを作成するまで完了

--- MyCommentViewer/MainWindow.xaml.cs
+++ MyCommentViewer/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,5 +27,59 @@ namespace MyCommentViewer
             InitializeComponent();
         }
 
+        private void ConnectOpenrec_Click(object sender, RoutedEventArgs e)
+        {
+            ConnectOpenrecMethod(OpenrecUrl.Text);
+        }
+
+        private void ConnectOpenrecMethod(string url)
+        {
+            string html = GetHtml(url);
+            string userId = OpenrecUserIdPurser(html);
+            string movieId = OpenrecMovieIdPurser(userId);
+            string websocketUrl = $"wss://chat.openrec.tv/socket.io/?movieId={movieId}&EIO=3&transport=websocket";
+            
+            //Console.WriteLine(movieId);
+        }
+
+        /// <summary>
+        /// 配信者のユーザーIDからapiのURLを作成し、配信ページのmovie_idをパースする事で取得
+        /// </summary>
+        /// <param name="userId">配信者のユーザーID</param>
+        /// <returns>配信ページのmovie_id</returns>
+        private string OpenrecMovieIdPurser(string userId)
+        {
+            string apiUrl = $"https://public.openrec.tv/external/api/v5/movies?channel_ids={userId}&sort=onair_status&is_upload=false";
+            string movieId = GetHtml(apiUrl);
+            movieId = movieId.Substring(movieId.IndexOf("movie_id"));
+            movieId = movieId.Replace("movie_id\":", "");
+            movieId = movieId.Substring(0, movieId.IndexOf(","));
+            return movieId;
+        }
+
+        /// <summary>
+        /// 配信ページのHTMLをパースし、配信者のユーザーIDを取得
+        /// </summary>
+        /// <param name="html">配信ページのHTML</param>
+        /// <returns>配信者のユーザーID</returns>
+        private string OpenrecUserIdPurser(string html)
+        {
+            string userId = html.Substring(html.IndexOf("],\"channel\":{\"id\":\""));
+            userId = userId.Replace("],\"channel\":{\"id\":\"", "");
+            userId = userId.Substring(0, userId.IndexOf("\""));
+            return userId;
+        }
+
+        /// <summary>
+        /// 引数にとったURLのHTMLを文字列で取得
+        /// </summary>
+        /// <param name="url">配信ページのURL</param>
+        /// <returns>配信ページのHTML</returns>
+        private string GetHtml(string url)
+        {
+            WebClient wc = new WebClient();
+            var html = wc.DownloadString(url);
+            return html;
+        }
     }
 }
